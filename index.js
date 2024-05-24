f2f0de3e594a94f74666f91c67f8cb27fa48e55e
Ivan Dario <darivanarsa@gmail.com>
2024-05-24 12:10:00 -0500

    detalles icono wasa

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ const menu = document.querySelector(".menu")
 
 const nav = document.querySelector(".menu_desplegable")
 
+const iconWasa = document.querySelector(".fixed-span")
+
 menu.addEventListener("click", (event) => {
 event.preventDefault()
 
 if (nav.style.display === "none" || nav.style.display === "") {
     nav.style.display = "block";
     menu.style.backgroundImage = 'url("./img/cerrar.png")';
-  
+    iconWasa.style.opacity = "0.5";
   } else {
     nav.style.display = "none";
     menu.style.backgroundImage = 'url("./img/menu.svg")';
-  
+    iconWasa.style.opacity = "100%";
   }
 });
 
 nav.addEventListener ("click", () =>{
   nav.style.display = "none";
   menu.style.backgroundImage = 'url("./img/menu.svg")';
-
+  iconWasa.style.opacity = "100%";
 })
 
